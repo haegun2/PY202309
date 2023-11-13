249bf37685be2337b5a8e77b86c4a61ae86ba31c
haegun2 <gorjsking@kakao.com>
2023-11-13 19:44:31 +0900

    Week11 student_grade 개선

---
+++ challenge/week11/student_grade.py
@@ -0,0 +1,44 @@
+#student라는 클래스를 생성하여 각 학생들의 값을 여기에 저장
+class Student():
+    name = ""
+    korean = 0
+    math = 0
+    english = 0
+    
+    def get_average(self):
+        return (self.korean + self.math + self.english)/3
+
+#데이터를 불러와 인덱스 처리 후 인스턴스 만들고 각 값 저장, ins_name 에 ins_name 객체 저장      
+def load_data():
+    lines = open("./students.csv", "r", encoding="utf8").readlines()
+    for line in lines[1:]:
+        a = line.strip().split(",")
+        ins_name = Student()
+        ins_name.name = a[0]
+        ins_name.korean = float(a[1])
+        ins_name.math = float(a[2])
+        ins_name.english = float(a[3])
+        ins_list.append(ins_name)
+    return ins_list
+        
+ins_list = []
+load_data()
+stu_ave = []
+
+#인스턴스로 구해진 평균을, stu_ave 리스트에 각 값을 저장
+for i in ins_list:
+    a = i.get_average()
+    stu_ave.append(a)
+
+#학생 평균 점수를 출력
+print("-----학생들의 평균 점수-----")
+for i in range(len(ins_list)):
+    print(ins_list[i].name, "의 평균 점수는", stu_ave[i], "입니다.")
+
+#학생 평균 점수를 csv 파일로도 쓰기
+wr = open("./average.txt", 'w', encoding="utf8")
+wr.write("-----학생들의 평균 점수-----\n")
+for i in range(len(ins_list)):
+    data = (f"{ins_list[i].name}의 평균 점수는 {stu_ave[i]}입니다.\n")
+    wr.write(data)
+wr.close()
